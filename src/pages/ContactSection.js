db6bfa9e46f11ee0e8277f9c2bb0efb2cb65f1b2
hachiko008 <joshuagalang8@gmail.com>
2025-07-15 19:10:26 +0800

    redeploy issues

--- src/pages/ContactSection.js
+++ src/pages/ContactSection.js
@@ -1,10 +1,10 @@
 // /src/pages/contact.js (or /src/components/ContactSection.js if it's a component)
 import React from 'react';
 
-const ContactSection = ({ darkMode, portfolioData }) => {
-  const email = data?.contactEmail;
-  const github = data?.github;
-  const linkedin = data?.linkedin;
+const ContactSection = ({ darkMode, portfolioData = {} }) => {
+  const email = portfolioData?.contactEmail;
+  const github = portfolioData?.github;
+  const linkedin = portfolioData?.linkedin;
   
   return (
     <section id="contact" className={`py-16 shadow-md rounded-lg mx-auto max-w-4xl mt-8 mb-12 px-6 lg:px-4 ${darkMode ? 'bg-gray-900' : 'bg-gray-100'}`}>
