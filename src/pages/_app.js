d821c2a1c05d1ce3f455e6e2cdbac1d243b75bd1
hachiko008 <joshuagalang8@gmail.com>
2025-07-16 00:32:20 +0800

    meta tags update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,23 @@
+import Head from 'next/head';
 import "@/styles/globals.css";
-
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return(
+        <>
+      <Head>
+        <title>JGalang</title>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="author" content="JGalang"/>
+        <meta property="og:title" content="JGalang" />
+        <meta property="og:description" content="JGalang" />
+        <meta property="og:image" content="https://img.icons8.com/color/48/resume.png" />
+        <meta property="og:url" content="https://jgalang.com/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <link rel="icon" href="https://img.icons8.com/color/48/resume.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,19 +3,8 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html>
       <Head>
-        {/* Global Meta Tags */}
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="author" content="Your Name or Company"/>
-        <meta property="og:title" content="JGalang" />
-        <meta property="og:description" content="JGalang" />
-        <meta property="og:image" content="https://img.icons8.com/color/48/resume.png" />
-        <meta property="og:url" content="https://jgalang.com/" />
-        <meta property="og:type" content="website" />
-        <link rel="icon" href="https://img.icons8.com/color/48/resume.png" />
-
       </Head>
       <body>
         <Main />
